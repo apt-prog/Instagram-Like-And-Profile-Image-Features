99508ad5c578b8e177cbabbf1263fbcc4ba2f0b8
FenilVegad <fenilvegad300@gmail.com>
2025-02-26 19:04:36 +0530

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-let count = 0;
+let LikeCount = document.querySelector("#LikeCount");
+let LikeIcon = document.querySelector("#Likes");
+let imgClick = document.querySelector("#Icard_body");
+let DP = document.querySelector("#DPImage");
+let DPshow = document.querySelector("#DpShow");
+let Heart = document.querySelector("#Heart");
+let DPInnerImage = document.querySelector(".FullScreen");
+let likeCount = 0;
 
-document.getElementById("Main").addEventListener("dblclick", function () {
-  count++;
-  console.log(count);
-  document.querySelector(".h1").innerText = count
-  let body = document.querySelector(".body");
-  body.style.transform = "scale(1)";
+imgClick.addEventListener("dblclick", function () {
+  ++likeCount;
+  LikeCount.innerHTML = likeCount;
 
+  if (likeCount === 0) {
+    console.log("Not Have");
+  } else {
+    Heart.className = "bi bi-heart-fill OnLink";
+  }
+
+  LikeIcon.style.scale = "1";
   setTimeout(() => {
-    body.style.transform = "scale(1.5)";
-    setTimeout(() => {
-      body.style.transition = ".5s ease-in-out";
-      body.style.transform = "scale(0)";
-    }, 400);
+    LikeIcon.style.scale = "1.45";
+    LikeIcon.style.transition = "0.55s ease-in-out";
+  }, 500);
+  setTimeout(() => {
+    LikeIcon.style.scale = "0";
   }, 1000);
 });
+
+Heart.addEventListener("click", function () {
+  ++likeCount;
+  LikeCount.innerHTML = likeCount;
+
+  if (likeCount === 0) {
+    console.log("Not Have");
+  } else {
+    Heart.className = "bi bi-heart-fill OnLink";
+  }
+
+  LikeIcon.style.scale = "1";
+  setTimeout(() => {
+    LikeIcon.style.scale = "1.45";
+    LikeIcon.style.transition = "0.55s ease-in-out";
+  }, 500);
+  setTimeout(() => {
+    LikeIcon.style.scale = "0";
+  }, 1000);
+});
+
+DP.addEventListener("click", function () {
+  DPshow.style.display = "inline";
+});
+
+DPInnerImage.addEventListener("click", function () {
+  DPshow.style.display = "none";
+});
+
